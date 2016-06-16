a99bcad99ee957f64a65f7b3cc6e2e78c710fda5
Axel Forsman <axelsfor@gmail.com>
2016-06-16 15:11:47 +0200

    Fix bug where wrong result would be returned.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /**
  * Return the next smallest multiple of @p x greater than @p i.
  *
+ * If @p x is equal to zero then @p i is returned.
  * @param i The integer that the result must be larger than.
  * @param x The integer that the result is a multiple of.
  * @return The next smallest multiple of @p x.
  */
 module.exports = function(i, x) {
-	return (i / x + 1) * x;
+	if (x === 0) return i;
+	return (i / x + 1 | 0) * x;
 };
